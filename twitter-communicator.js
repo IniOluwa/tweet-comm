e0c54afde88e612770e7165cf7c319122478b193
IniOluwa <inioluwafageyinbo@gmail.com>
2016-09-30 16:23:29 +0100

    Comments added.

--- twitter-communicator.js
+++ twitter-communicator.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+// Setting up twitter keys and tokens
 var Twit = require('twit')
 var twi = new Twit({
   consumer_key: process.env.consumer_key,
@@ -16,6 +17,7 @@ var twi = new Twit({
   timeout_ms: 60*1000,
 });
 
+// Prompting user to pick an option
 rl.question('Input 1 to search, 2 to post and 3 to get followers: ', (answer) => {
     var twitter = new tweetComm();
     if(answer == 1){
@@ -32,16 +34,18 @@ class tweetComm {
 
     }
 
+    // Search Twitter
     searchTwitter(){
         rl.question('Search twitter for?: ', (answer) => {
-            twi.get('search/tweets', { q: answer, count: 100 }, function(err, data, response) {
-                console.log(data)
+            twi.get('search/tweets', { q: answer, count: 1 }, function(err, data, response) {
+                console.log(data.statuses['text'])
+                console.log(data['statuses'])
                 })
             rl.close();
         });
     }
 
-
+    // Post a new tweet
     postTweet(){
 
         rl.question('Have a tweet in mind?: ', (answer) => {
@@ -52,7 +56,7 @@ class tweetComm {
         });
     }
 
-
+    // Get followers of a particular user
     getFollowers(){
         rl.question('Input a twitter handle to get the followers of the respective user: ', (answer) => {
             twi.get('followers/id', { screen_name: answer }, function(err, data, response) {
